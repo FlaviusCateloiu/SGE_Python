9330a906888810a219cb7242b7990bff8c7d5a53
flaars <fcateloiu08@gmail.com>
2022-11-23 17:28:00 +0100

    Parte de cadena terminado

--- Ejercicios Basicos Python/02Cadenas/ejercicio11.py
+++ Ejercicios Basicos Python/02Cadenas/ejercicio11.py
@@ -1,6 +1,6 @@
 producto = input("Introduce el nombre del producto: ")
 precioProd = float(input("Introduce el precio del producto: "))
 numUnidadesProd = int(input("Introduce el numero de unidades del producto: "))
-print('{producto}: {unidades:3d} unidades x {precio:6.2f}€ = {total:8.2f}€'.format(producto = producto, unidades = numUnidadesProd, precio = precioProd, total = numUnidadesProd * precioProd))
+print('{producto}: {precio:6.2f}€ x {unidades:3d} unidades = {total:8.2f}€'.format(producto = producto, unidades = numUnidadesProd, precio = precioProd, total = numUnidadesProd * precioProd))
 
 
