e7f0b2042f11a23dcf5024fdafb31395f956a79f
flaars <fcateloiu08@gmail.com>
2022-12-02 19:40:58 +0100

    Terminado los ejercicios Secillos python.

--- Ejercicios Basicos Python/05ListasYTuplas/ejercicio13.py
+++ Ejercicios Basicos Python/05ListasYTuplas/ejercicio13.py
@@ -1,3 +1,5 @@
+import math
+
 numeros = input("Introduce numeros separados por comas: ")
 listaNum = numeros.split(",")
 suma = 0
@@ -6,5 +8,5 @@ for i in range(len(listaNum)):
     suma += int(listaNum[i])
     sumsq += int(listaNum[i])**2
 media = suma/len(listaNum)
-desviacionTipica = (sumsq/len(listaNum)-media**2)**(1/2)
+desviacionTipica = math.sqrt((sumsq/len(listaNum)-media**2))
 print(f"La media es {media}, y la desviacion tipica es {desviacionTipica}")
