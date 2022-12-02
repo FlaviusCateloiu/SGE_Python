54e3593485fc985ee60365277f5f65855d184178
flaars <fcateloiu08@gmail.com>
2022-12-02 19:35:09 +0100

    Terminado los ejercicios Secillos python.

--- Ejercicios Basicos Python/05ListasYTuplas/ejercicio13.py
+++ Ejercicios Basicos Python/05ListasYTuplas/ejercicio13.py
@@ -4,7 +4,7 @@ suma = 0
 sumsq = 0
 for i in range(len(listaNum)):
     suma += int(listaNum[i])
-    sumsq += i**2
+    sumsq += int(listaNum[i])**2
 media = suma/len(listaNum)
 desviacionTipica = (sumsq/len(listaNum)-media**2)**(1/2)
 print(f"La media es {media}, y la desviacion tipica es {desviacionTipica}")
